b9d0794256243116938034dc2f1e0982eed5be75
최고요 <111892466+CHOIGOYO@users.noreply.github.com>
2022-12-03 05:17:51 +0900

    Delete java_standard_2/src directory

--- java_standard_2/src/Ex4_8.java
+++
@@ -1,8 +0,0 @@
-public class Ex4_8 {
-    public static void main(String[] args) {
-        // 반복문 for
-        for (int i =1 ; i <=3 ; i ++) {
-            System.out.println("Hello");
-        }
-    }
-}
